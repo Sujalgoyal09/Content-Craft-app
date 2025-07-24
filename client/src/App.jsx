68e3779b797e27dc89e4a77255be17162d0fe59c
Sujalgoyal09 <sujalgoyal09@gmail.com>
2025-07-24 21:04:22 +0530

    styled admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
@@ -12,18 +12,17 @@ export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path = '/' element = {<Home/>} />
-        <Route path = '/blog/:id' element = {<Blog/>} />
-        <Route path = '/admin' element = {<Layout/>}>
-          <Route index element = {<Dashboard/>}/>
-          <Route path = '/addBlog' element = {<AddBlog/>}/>
-          <Route path = '/listBlog' element = {<ListBlog/>}/>
-          <Route path = '/comments' element = {<Comments/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/blog/:id' element={<Blog />} />
+        <Route path='/admin' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='addBlog' element={<AddBlog />} />
+          <Route path='listBlog' element={<ListBlog />} />
+          <Route path='comments' element={<Comments />} />
         </Route>
       </Routes>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
